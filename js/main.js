ca226736ae20941cae0770e9513441aa92b39e04
Daniela Cohen <danielacohen@Danielas-MacBook-Pro.local>
2023-09-11 10:23:44 +0200

    hiding kittens

--- js/main.js
+++ js/main.js
@@ -2,58 +2,6 @@
 
 const listElement = document.querySelector ('.js-list');
 
-// const kittenOne = ` <li class="card">
-// <article>
-//   <img
-//     class="card_img"
-//     src="https://dev.adalab.es/gato-siames.webp"
-//     alt="siames-cat"
-//   />
-//   <h3 class="card_title">Anastacio</h3>
-//   <h4 class="card_race">Siamés</h4>
-//   <p class="card_description">
-//     Porte elegante, su patrón de color tan característico y sus ojos
-//     de un azul intenso, pero su historia se remonta a Asía al menos
-//     hace 500 años, donde tuvo su origen muy posiblemente.
-//   </p>
-// </article>
-// </li>`; 
-
-
-// const kittenTwo = `<li class="card">
-// <img
-//   class="card_img"
-//   src="https://dev.adalab.es/sphynx-gato.webp"
-//   alt="sphynx-cat"
-// />
-// <h3 class="card_title">Fiona</h3>
-// <h4 class="card_race">Sphynx</h4>
-// <p class="card_description">
-//   Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
-//   hasta con pinta de alienígena han llegado a definir a esta raza
-//   gatuna que se caracteriza por la «ausencia» de pelo.
-// </p>
-// </li>`;
-
-
-// const kittenThree = `<li class="card">
-// <img
-//   class="card_img"
-//   src="https://dev.adalab.es/maine-coon-cat.webp"
-//   alt="maine-coon-cat"
-// />
-// <h3 class="card_title">Cielo</h3>
-// <h4 class="card_race">Maine Coon</h4>
-// <p class="card_description">
-//   Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
-//   bella mirada se ha convertido en una de sus señas de identidad.
-//   Sus ojos son grandes y las orejas resultan largas y en punta.
-// </p>
-// </li>`;
-
-
-
-// listElement.innerHTML = kittenOne + kittenTwo + kittenThree;
 
 
 const kittenImageOne =  'https://dev.adalab.es/gato-siames.webp';
@@ -116,4 +64,23 @@ const kittenThree = `<li class="card">
 </p>
 </li>`;
 
-listElement.innerHTML = kittenOne + kittenTwo + kittenThree;
+
+
+const input_search_desc = document.querySelector ('.js_in_search_desc');
+
+const descrSearchText = input_search_desc.value;
+
+if( !kittenDescOne.includes(descrSearchText) ) {
+  kittenOne.classList.add ('hidden');
+  }
+  
+  if( !kittenDescTwo.includes(descrSearchText) ) {
+  kittenTwo.classList.add ('hidden');
+  }
+  
+  if( !kittenDescThree.includes(descrSearchText) ) {
+    kittenThree.classList.add ('hidden');
+  }
+  
+  listElement.innerHTML = kittenOne + kittenTwo + kittenThree;
+
